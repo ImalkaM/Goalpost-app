c620e1978b04a92e74aa4de79ad5999d0dcdf1d5
Imalka Udayanga <imalka.muthukumara@vitalhub.com>
2022-05-28 10:35:27 +0530

    Initial Commit

---
+++ goalpost-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goalpost-app
+//
+//  Created by Imalka Muthukumara on 2022-05-28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
